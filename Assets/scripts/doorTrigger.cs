286c1571b3f6abc3e77ae5ae200aa21916454b25
mahdi687 <mahdibennsir123@gmail.com>
2023-02-04 22:28:17 +0100

    update

--- Assets/scripts/doorTrigger.cs
+++ Assets/scripts/doorTrigger.cs
@@ -1,32 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class doorTrigger : MonoBehaviour 
 {
-    
+    public bool canpress =false; 
+    public bool canpress1 = false;
+    public bool collided=false;
+    public GameObject door;
+    public GameObject door1;
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.E) && canpress && collided)
+        {
+
+            door.SetActive(false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.E) && canpress1 && collided)
+        {
+
+            door1.SetActive(false);
+
+        }
+
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.E) && collided && !canpress && !canpress1)
+        {
+            
+           
+            StartCoroutine(delai());
+        }
+    }
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("beb" ))
+
+
+        if (other.gameObject.CompareTag("s7y7"))
         {
-            Debug.Log(" beeeb sahyt ");
-            if(other.gameObject.CompareTag("s7y7"))
+            collided = true;
+            canpress = true;
+
+        }
+        if (other.gameObject.CompareTag("s7y71"))
+        {
+            collided = true;
+            canpress1 = true;
+
+        }
+        if (other.gameObject.CompareTag("mouch s7y7"))
             {
 
-            }
-            else if (other.gameObject.CompareTag("mouch s7y7"))
-            {
-                
-            }
+            collided=true;
+           
+
         }
       
 
     }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("s7y7"))
+        {
+            canpress = false;
+           
+        }
+        if (other.gameObject.CompareTag("s7y71"))
+        {
+            canpress1 = false;
 
+        }
+    }
     IEnumerator delai()
     {
         yield return new WaitForSeconds(2);
-        SceneManager.GetActiveScene();
+        SceneManager.LoadScene(0);
     }
 }
