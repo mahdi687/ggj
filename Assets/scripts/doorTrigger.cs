9f97808e06522ea5d794f96f92b3a4af4dda2099
mahdi687 <mahdibennsir123@gmail.com>
2023-02-04 20:28:32 +0100

    update

--- Assets/scripts/doorTrigger.cs
+++ Assets/scripts/doorTrigger.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class doorTrigger : MonoBehaviour 
 {
     
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("s7y7" ))
+        if(other.gameObject.CompareTag("beb" ))
         {
-            Debug.Log(" sa7yyt ye 9a7ba ");
+            Debug.Log(" beeeb sahyt ");
+            if(other.gameObject.CompareTag("s7y7"))
+            {
 
-        }
-        else
-        {
-            Debug.Log("zab ?? ");
+            }
+            else if (other.gameObject.CompareTag("mouch s7y7"))
+            {
+                
+            }
         }
       
 
+    }
+
+    IEnumerator delai()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.GetActiveScene();
     }
 }
